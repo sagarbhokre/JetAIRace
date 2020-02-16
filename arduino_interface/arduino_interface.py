1ab281556b631334d9a090aad3b93227c9e44a94
Sagar Bhokre <sagarbhokre@gmail.com>
2020-02-16 01:28:09 -0800

    Jetson-Arduino interface add retry connection logic
    + Handles cases where arduino disconnects and reconnects
    + Fix and stability improvements

--- arduino_interface/arduino_interface.py
+++ arduino_interface/arduino_interface.py
@@ -2,27 +2,55 @@ import serial
 import time
 
 serport = "/dev/ttyACM0"
-
-ser = serial.Serial(serport,115200)
-ser.flushInput()
+ser = None
 
 DEBUG = 1
 
 def write_ser(data):
+    global ser
     ser.write(bytes(data, 'utf-8'))
     ser.flush()
 
 def read_ser():
-    return ser.readline().decode("utf-8")
+    global ser
+    serData = ser.readline()
+    if len(serData) > 0 and serData[-1] == 10:
+        return True, serData.decode("utf-8")
+
+    return False, 'A'
+
+def init_ser():
+    global ser
+    ser = serial.Serial(serport,115200, timeout=.4)
+    ser.flushInput()
+
+def retry_setup_connection():
+    global ser
+    count = 10
+    if ser is not None:
+        ser.close()
+
+    while count > 0:
+        if setup_connection():
+            return True
+        count -= 1
+
+    return False
 
 def setup_connection():
+    print("Trying to setup connection")
+    init_ser()
     write_ser("A\n");
     write_ser("A\n");
     write_ser("A\n");
     write_ser("A\n");
 
-    data = read_ser()
-    arduinoRet = data.strip().split(' ')[-1]
+    ret, data = read_ser()
+    if(ret == True):
+        arduinoRet = data.strip().split(' ')[-1]
+    else:
+        return False
+
     if DEBUG: print("Read from %s: --%s--"%(serport, data))
 
     if arduinoRet == 'A':
@@ -37,7 +65,7 @@ def send_actuate(lDir, lSpeed, rDir, rSpeed):
 if __name__ == '__main__':
     count = 0
     direction = 0
-    if setup_connection():
+    if retry_setup_connection():
         while True:
             count = (count + 1) % 10
             if count == 0:
@@ -47,7 +75,11 @@ if __name__ == '__main__':
 
             send_actuate(lDir, 9-count, rDir, count)
             if DEBUG: time.sleep(.3)
-            line = read_ser().strip()
-            if DEBUG: print("Read from %s: %s"%(serport, line))
+            ret, line = read_ser()
+            if ret == False:
+                print("Read failed retrying connection")
+                retry_setup_connection()
+
+            if DEBUG: print("Read from %s: %s"%(serport, line.strip()))
     else:
         print("Setup failed")
